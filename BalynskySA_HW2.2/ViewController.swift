812e24ff2532b068c2cd08917d9e28e3b0931f0d
Balynsky <sbalynsky@yandex.ru>
2020-05-22 01:36:17 +0300

    Release_V1

--- BalynskySA_HW2.2/ViewController.swift
+++ BalynskySA_HW2.2/ViewController.swift
@@ -22,30 +22,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     
     
-
     override func viewDidLoad() {
-    
+        
         super.viewDidLoad()
-        lebelRedText.text = "Red"
-        lebelGreenText.text = "Green"
-        lebelBlueText.text = "Blue"
+        lebelRedText.text = "Red:"
+        lebelGreenText.text = "Green:"
+        lebelBlueText.text = "Blue:"
         
         lebelRedValue.text = String(format: "%.2f", redSlider.value)
         lebelGreenValue.text = String(format: "%.2f", greenSlider.value)
         lebelBlueValue.text = String(format: "%.2f", blueSlider.value)
     }
-
+    
     @IBAction func redSliderAction() {
         lebelRedValue.text = String(format: "%.2f", redSlider.value)
-        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value),
+                                        green: CGFloat(greenSlider.value),
+                                        blue: CGFloat(blueSlider.value),
+                                        alpha: 1)
     }
     @IBAction func greenSliderAction() {
         lebelGreenValue.text = String(format: "%.2f", greenSlider.value)
-        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value),
+                                        green: CGFloat(greenSlider.value),
+                                        blue: CGFloat(blueSlider.value),
+                                        alpha: 1)
     }
     @IBAction func blueSliderAction() {
         lebelBlueValue.text = String(format: "%.2f", blueSlider.value)
-        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value),
+                                        green: CGFloat(greenSlider.value),
+                                        blue: CGFloat(blueSlider.value),
+                                        alpha: 1)
     }
     
 }
