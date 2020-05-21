db2ecaeae4cb28870ec5b60fc506968e24a96439
Balynsky <sbalynsky@yandex.ru>
2020-05-22 01:10:15 +0300

    MPV

--- BalynskySA_HW2.2/ViewController.swift
+++ BalynskySA_HW2.2/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewRGB: UIView!
+    @IBOutlet weak var lebelRedText: UILabel!
+    @IBOutlet weak var lebelGreenText: UILabel!
+    @IBOutlet weak var lebelBlueText: UILabel!
+    @IBOutlet weak var lebelRedValue: UILabel!
+    @IBOutlet weak var lebelGreenValue: UILabel!
+    @IBOutlet weak var lebelBlueValue: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
 
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lebelRedText.text = "Red"
+        lebelGreenText.text = "Green"
+        lebelBlueText.text = "Blue"
+        
+        lebelRedValue.text = String(format: "%.2f", redSlider.value)
+        lebelGreenValue.text = String(format: "%.2f", greenSlider.value)
+        lebelBlueValue.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    @IBAction func redSliderAction() {
+        lebelRedValue.text = String(format: "%.2f", redSlider.value)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func greenSliderAction() {
+        lebelGreenValue.text = String(format: "%.2f", greenSlider.value)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func blueSliderAction() {
+        lebelBlueValue.text = String(format: "%.2f", blueSlider.value)
+        viewRGB.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
 }
 
